4307ddc6f170cf07065281d50b14191beb1f7a61
Eric Lee <hownl96@gmail.com>
2023-08-05 02:45:42 -0500

    feat: add hamburger menu bar

--- resources/views/components/layouts/site/header.blade.php
+++ resources/views/components/layouts/site/header.blade.php
@@ -8,7 +8,14 @@
         <img src="{{ asset('images/logo.svg') }}" alt="Logo" class="w-60 h-auto" style="display: none;" onload="this.style.display='inline';">
     </a>
 
-    <nav class="mr-4">
+    <!-- Mobile Hamburger Menu Icon -->
+    <button class="lg:hidden px-2 py-1 text-gray-800 focus:outline-none" id="menuBtn">
+        <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+        </svg>
+    </button>
+
+    <nav class="mr-4 hidden lg:block" id="desktopNav">
         <ul class="flex space-x-4">
             <li>
                 <a href="{{ route('home') }}" class="{{ $aboutActive ? 'active' : '' }} rounded-full py-2 px-4 text-black hover:text-blue-500 hover:bg-blue-100">Home</a>
@@ -19,3 +26,25 @@
         </ul>
     </nav>
 </header>
+
+<!-- Mobile Navigation Menu -->
+<div class="hidden lg:hidden" id="mobileNav">
+    <ul class="py-2 px-4 bg-gray-200">
+        <li>
+            <a href="{{ route('home') }}" class="{{ $aboutActive ? 'active' : '' }} block py-2 px-4 text-black hover:text-blue-500 hover:bg-blue-100">Home</a>
+        </li>
+        <li>
+            <a href="{{ route('discount') }}" class="{{ $discountActive ? 'active' : '' }} block py-2 px-4 text-black hover:text-blue-500 hover:bg-blue-100">Discount Slasher</a>
+        </li>
+    </ul>
+</div>
+
+<!-- Add JavaScript to toggle mobile navigation -->
+<script>
+    const menuBtn = document.getElementById('menuBtn');
+    const mobileNav = document.getElementById('mobileNav');
+
+    menuBtn.addEventListener('click', () => {
+        mobileNav.classList.toggle('hidden');
+    });
+</script>
